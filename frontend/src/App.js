f888f1b2197b291219ac9ce24dbc278d8f8b8797
Cher-Study <mainsail_645@inbox.ru>
2022-04-13 19:30:52 +0300

    homework2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import UserList from './components/UserList.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'users': []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://127.0.0.1:8000/api/users/')
+      .then(response => {
+        let users = response.data
+        this.setState({
+          'users': users
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <div class='menu'></div>
+
+        <UserList users={this.state.users} />
+
+        <div class='footer'></div>
+      </div>
+    )
+  }
 }
 
 export default App;
