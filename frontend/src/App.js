801f7e3a6e32b513f9dbe449889c81f837ec1dee
Cher-Study <mainsail_645@inbox.ru>
2022-05-05 21:51:04 +0300

    frontend was deleted and created again. It works!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,35 @@
 import React from 'react'
 import axios from 'axios'
+import './App.css';
 import UserList from './components/UserList.js'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'users': []
+    constructor(props) {
+        super(props)
+        this.state = {
+            'users': []
+        }
     }
-  }
 
-  componentDidMount() {
-    axios
-      .get('http://127.0.0.1:8000/api/users/')
-      .then(response => {
-        let users = response.data.results
-        this.setState({
-          'users': users
-        })
-      })
-      .catch(error => console.log(error))
-  }
+    componentDidMount() {
+        axios
+            .get('http://127.0.0.1:8000/api/users/')
+            .then(response => {
+                let users = response.data.results
+                this.setState({
+                    'users': users
+                })
+            })
+            .catch(error => console.log(error))
+    }
 
-  render() {
-    return (
-      <div>
-        <div class='menu'></div>
-
-        <div> <UserList users={this.state.users} /> </div>
-
-        <div class='footer'></div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users} />
+            </div>
+        )
+    }
 }
 
 export default App;
